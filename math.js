d0c7b1bf93e5764baeb8b6252a0ee9ddc3eb2692
Wesley Alves do Nascimento <65147671+Wesley708@users.noreply.github.com>
2025-02-25 00:56:56 +0000

    Correção da multiplicação

--- math.js
+++ math.js
@@ -7,7 +7,7 @@ const subtract = function(a,b){
     return a - b;
 }
 
-const multiply = (a,b) => {a*b}
+const multiply = (a,b) => {return a*b}
 
 function divide(a,b){
     return a / b;
--- tests/math.test.js
+++ tests/math.test.js
@@ -9,7 +9,7 @@ test('subtração 5 - 2 igual a 3', () =>{
 })
 
 test('multplicação 5 x 3 igual a 15',()=>{
-    expect(math.multiply(5, 3)).toBe(undefined);
+    expect(math.multiply(5, 3)).toBe(15);
 })
 
 test('divisão 10 / 2 iagual a 5',()=>{
